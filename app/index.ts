b3d58e94f204d30819591c82e1e445ce3c8f94ff
Aliona Butkevich <aliona_butkevich@epam.com>
2020-02-21 14:43:32 +0300

    added global error handling

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,27 @@
-import express from "express";
-import { errors } from "celebrate";
+import express, { Request, Response } from "express";
 import bunyan from "bunyan";
 
-import routes from "./routes";
+import { errors } from "celebrate";
+import { createLogger, transports } from "winston";
+
 import { syncWithDB } from "./data-access/db";
+import routes from "./routes";
 
 const port = 3000;
 
 const app = express();
+
+export const logger = createLogger({
+  transports: [
+    new transports.Console(),
+  ],
+  exceptionHandlers: [
+    new transports.Console(),
+  ],
+  exitOnError: false,
+});
+
+export const serviceLogger = bunyan.createLogger({ name: "serviceLogger" });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -16,10 +30,19 @@ app.use("/", routes);
 
 app.use(errors());
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 syncWithDB();
 
-export const serviceLogger = bunyan.createLogger({ name: "serviceLogger" });
+app.use((err: Error, req: Request, res: Response) => {
+  logger.log({ level: "error", message: err.message });
+  res.end(500);
+});
+
+process.on('unhandledRejection', () => {
+  logger.log({ level: "error", message: "unhandled rejection" });
+}).on('uncaughtException', err => {
+  logger.log({ level: "error", message: err.message });
+});
+
+app.listen(port, () => {
+  logger.log({ level: "info", message: `Server is running on port ${port}` });
+});
